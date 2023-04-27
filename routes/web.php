aed7ed1707e618721ce0ff57071fd2ceca639e5f
David <david.katalinic9@gmail.com>
2023-04-27 18:06:00 +0200

    User routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Show register form for a user
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+//Store user
+Route::post('/users', [UserController::class, 'store']);
+
+//Show login form for a user
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+//Authenticate user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
+//Logout user
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+//Show edit form
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->middleware('auth');
+
+//Update user
+Route::put('/users/{user}', [UserController::class, 'update']);
+
+//delete user
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth');
