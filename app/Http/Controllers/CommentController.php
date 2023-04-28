e269545f6ce1fd8481a5353d26988dfa2a969e60
David <david.katalinic9@gmail.com>
2023-04-29 00:11:42 +0200

    store function for comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request){
+        $formFields = $request->validate([
+            'comment' => 'required'
+        ]);
+
+        $formFields['user_id'] = auth()->user()->id;
+        $formFields['ticket_id'] = $request->ticket_id;
+
+        Comment::create($formFields);
+
+        return back()->with('message', 'Comment created');
+    }
 }
