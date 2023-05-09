8d651c9579d58520429e4c0cef855dd02b9ae06f
David <david.katalinic9@gmail.com>
2023-05-09 12:11:31 +0200

    Navbar buttons for user

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -64,16 +64,30 @@
         <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
             <div class="w-1/2"></div>
             <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-                <button @click="isOpen = !isOpen" class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
-                    <img src="https://source.unsplash.com/uJ8LNVCBjFQ/400x400">
-                </button>
-                <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
-                <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
-                    <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
-                    <a href="#" class="block px-4 py-2 account-link hover:text-white">Sign Out</a>
-                </div>
+                @auth
+                    <button @click="isOpen = !isOpen" class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
+                        <span>{{ Str::limit(auth()->user()->name, 1, '') }}</span>
+                    </button>
+                    <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
+                    <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
+                        <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
+                        <form class="block px-4 py-2 account-link hover:text-white" method="POST" action="/logout">
+                            @csrf
+                            <button type="submit">
+                            Logout
+                            </button>
+                          </form>
+                  
+                    
+                    </div>
+                @else
+                    <a href="/login" class="block px-4 py-2 hover:text-blue-600"> <i class="fas fa-sign-in-alt"></i> Login</a>
+                    <a href="/register" class="block px-4 py-2 hover:text-blue-600"><i class="fas fa-user-plus"></i> Register</a>
+                @endauth
             </div>
         </header>
+        
+        
 
         <!-- Mobile Header & Nav -->
         <header x-data="{ isOpen: false }" class="w-full bg-sidebar py-5 px-6 sm:hidden">
