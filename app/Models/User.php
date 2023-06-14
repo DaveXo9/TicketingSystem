fcc5e2bb72f2bf0951ced60efa89fa59e0d47855
David <david.katalinic9@gmail.com>
2023-06-14 11:54:53 +0200

    Messages model and table

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,8 @@ class User extends Authenticatable
     {
     return $this->hasMany(Notification::class);
     }
+
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
 }
